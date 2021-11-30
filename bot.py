096498021b963a38766d9cb8fc7dcafbe09116b3
buxorotech <95276000+buxorotech@users.noreply.github.com>
2021-11-30 16:21:06 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from googletrans import Translator
 updater = Updater('2115820611:AAHHGQm4ivUvsW94EFk-DCuRXRuW1F55LkU',use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('hi iam google translater ')
+ updater.message.reply_text('🖖Salom, Men Tarjimon botman🤖 Tarjima qilmoqchi bolgan sozingizni menga jonating! ')
  
 def echo(updater,context):
  usr_msg =updater.message.text
  translator = Translator()
- translation = translator.translate(usr_msg,dest='hi') 
+ translation = translator.translate(usr_msg,dest='uz') 
  updater.message.reply_text(translation.text)
  
 dp =updater.dispatcher.add_handler
